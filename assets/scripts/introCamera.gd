d0d99fb335a9d1ff6fe5526825c19bd75b27710a
TheFoppian <38819438+TheFoppian@users.noreply.github.com>
2023-09-30 18:52:19 -0500

    Fixed intro scene with new viewport size

--- assets/scripts/introCamera.gd
+++ assets/scripts/introCamera.gd
@@ -1,6 +1,6 @@
 extends Camera2D
 
 func _process(delta):
-	zoom *= 1.0001
-	position = Vector2(800, 450 + (0.5 * (DisplayServer.window_get_size().y - (get_viewport().get_visible_rect().size.y / zoom.y))));
+	zoom *= 1.001
+	position = Vector2(500, 500 + (0.2 * (DisplayServer.window_get_size().y - (get_viewport().get_visible_rect().size.y / zoom.y))));
 	print((get_viewport().get_visible_rect().size.y / zoom.y));
