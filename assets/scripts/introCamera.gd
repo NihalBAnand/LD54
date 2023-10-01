9d8f9a48a1ed84842058e5d00f95393833f6bd46
TheFoppian <38819438+TheFoppian@users.noreply.github.com>
2023-10-01 16:29:47 -0500

    Intro is now main scene

--- assets/scripts/introCamera.gd
+++ assets/scripts/introCamera.gd
@@ -6,5 +6,4 @@ func _process(delta):
 	zoom *= 1 + (0.0001 * zoomAccel);
 	position = Vector2(1000, 500 + (0.2 * (DisplayServer.window_get_size().y - (get_viewport().get_visible_rect().size.y / zoom.y))));
 	zoomAccel += 0.01;
-	print((get_viewport().get_visible_rect().size.y / zoom.y));
 
